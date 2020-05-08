cabf1d0021bb5c2f1f1dd35b1ae9f84444157cc1
Nathan Johnson <nathanjohnson@Nathans-MBP.lan>
2020-05-08 11:45:20 -0500

    form

--- src/templates/template.js
+++ src/templates/template.js
@@ -10,6 +10,7 @@ import eBackground from "../images/ebackground.jpg"
 import junk from "../images/junk.jpg"
 import junkpile from "../images/junkpile.jpg"
 import crane from "../images/crane.jpg"
+import Picture from "../images/ewaste-pic.jpg"
 
 // icons
 import faceIcon from "../images/Icons/faceiconBlack.png"
@@ -19,6 +20,8 @@ import recycle from "../images/Icons/recycle.png"
 import Card from "@material-ui/core/Card"
 import Typography from "@material-ui/core/Typography"
 import CardContent from "@material-ui/core/CardContent"
+import Button from "@material-ui/core/Button"
+import TextField from "@material-ui/core/TextField"
 
 //styles
 const styles = theme => ({
@@ -87,16 +90,280 @@ const styles = theme => ({
     paddingBottom: "56.25%",
     color: "red",
   },
+  city: {
+    fontSize: "calc(.3rem + 2vw)",
+  },
+  wanted1: {
+    width: 1200,
+    position: "relative",
+    textAlign: "center",
+  },
+  wantedPic: {
+    width: 800,
+    margin: "0 auto",
+    display: "block",
+  },
+  form: {
+    width: "50%",
+    margin: "0 auto",
+  },
+  centered: {
+    margin: 20,
+    textAlign: "center",
+  },
 })
 
-
 class Template extends Component {
-
   render() {
+    const { city } = this.props.pageContext
     const { classes } = this.props
-    return (<Layout>
-   the city
-      </Layout>)
+    return (
+      <Layout>
+        <SEO
+          title={`Electronics and E-Waste Recycling ${city.name}, ${city.state}`}
+          description={`Computer, server, desktop, controllers, PLC, Recycling ${city.name}, ${city.state}`}
+        />
+
+        <div className={classes.header1}>
+          <div className={classes.wanted}>
+            <h3>Industrial Electronics and Computer Removal and Recycling</h3>
+          </div>
+          <div className={classes.wanted}>
+            <h2>
+              E-Waste {city.name}, {city.state}
+            </h2>
+          </div>
+          <Grid container className={classes.root} spacing={2}>
+            <Grid item xs={12}>
+              <Grid
+                container
+                direction="row"
+                justify="center"
+                alignItems="center"
+                spacing={3}
+              >
+                <Grid item></Grid>
+                <Grid item>
+                  <img
+                    className={classes.img}
+                    src={recycle}
+                    alt="Recycle, Industrial, Electronics, E Waste"
+                  />
+                </Grid>
+                <Grid item></Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </div>
+
+        <div className="section">
+          <div className="section1">
+            <div className="firstchilds">
+              <h1>
+                E-waste
+                <div> Removal&nbsp;& </div>
+                Recycling
+                <div className={classes.city}>{city.name}</div>
+              </h1>
+            </div>
+            <Grid
+              container
+              spacing={2}
+              direction="row"
+              justify="center"
+              alignItems="center"
+            >
+              <Grid
+                item
+                sm={4}
+                xs={12}
+                component={Link}
+                to="/contact"
+                style={{ textDecoration: "none", zIndex: 3 }}
+              >
+                <div className="block1">
+                  <img src={faceIcon} alt="NJT Automation Equipment"></img>
+                  <div className="text">
+                    Contact
+                    <div>NJT </div>
+                  </div>
+                </div>
+              </Grid>
+            </Grid>
+          </div>
+        </div>
+
+        <div className="body">
+          <div className="divider"></div>
+
+          <div className="wanted1">
+            <Link
+              to="/contact"
+              style={{ textDecoration: "none", textAlign: "center" }}
+            >
+              <h1> RECYCLE ELECTRONICS PROPERLY</h1>
+              <h4>Serving {city.county} County</h4>
+
+              <img
+                src={Picture}
+                className={classes.wantedPic}
+                alt={`Computer recycling ${city.name}, ${city.state}`}
+              ></img>
+            </Link>
+          </div>
+          <div className="divider"></div>
+          <Grid
+            container
+            spacing={2}
+            direction="row"
+            justify="center"
+            alignItems="center"
+          >
+            <Grid item sm={4} xs={12} className={classes.cards}>
+              <Card className={classes.card}>
+                <CardContent>
+                  <Typography
+                    className={classes.title}
+                    color="textSecondary"
+                    gutterBottom
+                  >
+                    INDUSTRIAL E-WASTE
+                  </Typography>
+                  <Typography className={classes.pos} color="textSecondary">
+                    NJT industrial e-waste disposal service includes: the
+                    collection and recycling of non-working electronics, scrap
+                    circuit boards, manufacturing e-waste, electrical machinery,
+                    automation components and equipment to be recycled.{" "}
+                  </Typography>
+
+                  <img
+                    src={junkpile}
+                    className={classes.wasteImages}
+                    alt="Automation Junk"
+                  ></img>
+                </CardContent>
+              </Card>
+            </Grid>
+
+            <Grid item sm={4} xs={12} className={classes.cards}>
+              <Card className={classes.card}>
+                <CardContent>
+                  <Typography
+                    className={classes.title}
+                    color="textSecondary"
+                    gutterBottom
+                  >
+                    OUT-DATED AUTOMATION
+                  </Typography>
+                  <Typography className={classes.pos} color="textSecondary">
+                    Waste electronics, obsolete or broken electrical machinery,
+                    production scrap metals and plastics resulting from
+                    manufacturing, can easily be fully recycled 99% of the time.
+                    Saving costs while reducing waste and harm to the
+                    environment.
+                  </Typography>
+                  <img
+                    src={junk}
+                    className={classes.wasteImages}
+                    alt="Sell Electronic E Waste"
+                  ></img>
+                </CardContent>
+              </Card>
+            </Grid>
+
+            <Grid item sm={4} xs={12} className={classes.cards}>
+              <Card className={classes.card}>
+                <CardContent>
+                  <Typography
+                    className={classes.title}
+                    color="textSecondary"
+                    gutterBottom
+                  >
+                    REMOVAL SERVICE
+                  </Typography>
+                  <Typography className={classes.pos} color="textSecondary">
+                    E-Waste is a world-wide issue and can be toxic to the
+                    environment if not handled properly. NJT is partners with
+                    several environmentally-conscious recycling centers. Items
+                    will be disposed of to the proper establishment.
+                  </Typography>
+                  <img
+                    src={crane}
+                    className={classes.wasteImages}
+                    alt="Sell Electronic E Waste"
+                  ></img>
+                </CardContent>
+              </Card>
+            </Grid>
+          </Grid>
+
+          <div>
+            <form
+              action={`mailto:nathan@njtautomation.com?subject=E-Waste - ${city.name}`}
+              method="post"
+              enctype="text/plain"
+              className={classes.form}
+            >
+              <h2> Submit An E-Waste Request: </h2>
+              <TextField
+                name="Name"
+                type="text"
+                label="Name / Company"
+                multiline
+                rows="2"
+                className={classes.textField}
+                placeholder="Name"
+                onChange={this.handleChange}
+                fullWidth
+              />
+              <TextField
+                name="Description"
+                type="text"
+                label="Description of Items to Be Recycled"
+                multiline
+                rows="2"
+                className={classes.textField}
+                placeholder="Description"
+                onChange={this.handleChange}
+                fullWidth
+              />
+              <TextField
+                name="Email Address"
+                type="text"
+                label="Email Address"
+                placeholder="Email"
+                multiline
+                rows="2"
+                className={classes.textField}
+                onChange={this.handleChange}
+                fullWidth
+              />
+              <TextField
+                name="Location"
+                type="text"
+                label="Location"
+                placeholder="Location"
+                multiline
+                rows="2"
+                className={classes.textField}
+                onChange={this.handleChange}
+                fullWidth
+              />
+              <div className={classes.centered}>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  color="primary"
+                  className={classes.Button}
+                >
+                  Submit
+                </Button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </Layout>
+    )
   }
 }
 
